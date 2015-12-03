47663e7713c36a69257a060a6efb59b22890abb0
Kai Dev Hu <kdhu@thoughtworks.com>
2015-12-03 11:09:57 +0800

    make field price final

--- src/main/java/com/thoughtworks/pos/Item.java
+++ src/main/java/com/thoughtworks/pos/Item.java
@@ -2,16 +2,11 @@ package com.thoughtworks.pos;
 
 public final class Item {
     private final String barcode;
-    private double price;
+    private final double price;
 
     public Item(String barcode, double price) {
         this.barcode = barcode;
         this.price = price;
-    }
-
-    public Item setPrice(double price) {
-        this.price = price;
-        return this;
     }
 
     public String getBarcode() {
